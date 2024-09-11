c833f130db6727ba0fae295849910d130f63caa8
PharoGG <timick6666@gmail.com>
2024-09-11 18:12:29 +0300

    fixes prettier, fixed bug with categories in CardForm

--- src/components/CardForm.vue
+++ src/components/CardForm.vue
@@ -7,59 +7,69 @@
         type="text"
         label="Введите ссылку на изображение"
         variant="underlined"
-        style="width: 400px; margin-top: 20px">
+        style="width: 400px; margin-top: 20px"
+      >
       </v-text-field>
       <v-text-field
         v-model="form.id"
         type="text"
         label="Введите ID товара"
         variant="underlined"
-        style="width: 400px">
+        style="width: 400px"
+      >
       </v-text-field>
       <v-text-field
         v-model="form.title"
         type="text"
         label="Введите заголовок товара"
         variant="underlined"
-        style="width: 400px">
+        style="width: 400px"
+      >
       </v-text-field>
       <v-text-field
         v-model="form.description"
         type="text"
         label="Введите описание товара"
         variant="underlined"
-        style="width: 400px">
+        style="width: 400px"
+      >
       </v-text-field>
       <v-autocomplete
-        :value="form.category"
+        v-model="form.category"
         :items="categories"
         label="Выберите категорию товара"
         variant="underlined"
-        style="width: 400px">
+        style="width: 400px"
+      >
       </v-autocomplete>
       <v-text-field
         v-model="form.price"
         type="number"
         label="Введите цену"
         variant="underlined"
-        style="width: 400px">
+        style="width: 400px"
+      >
       </v-text-field>
       <v-text-field
         v-model="form.rating.rate"
         type="number"
         label="Введите рейтинг"
         variant="underlined"
-        style="width: 400px">
+        style="width: 400px"
+      >
       </v-text-field>
       <v-text-field
         v-model="form.rating.count"
         type="number"
         label="Введите колличество баллов"
         variant="underlined"
-        style="width: 400px">
+        style="width: 400px"
+      >
       </v-text-field>
       <v-card-actions class="justify-space-between" style="width: 400px">
-        <v-btn variant="outlined" color="green" @click="$emit('saveCard')">Сохранить</v-btn>
+        <v-btn variant="outlined" color="green" @click="$emit('saveCard')"
+          >Сохранить</v-btn
+        >
         <v-btn variant="outlined" @click="$emit('closeForm')">Отменить</v-btn>
       </v-card-actions>
     </v-card>
@@ -67,13 +77,18 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  defineProps({
-    title: '',
-    form: {},
-  });
+import { ref } from "vue";
+defineProps({
+  title: "",
+  form: {},
+});
 
-  const categories = ref([`men's clothing`, 'jewelery', 'electronics', `women's clothing`]);
+const categories = ref([
+  `men's clothing`,
+  "jewelery",
+  "electronics",
+  `women's clothing`,
+]);
 </script>
 
 <style lang="scss" scoped></style>
